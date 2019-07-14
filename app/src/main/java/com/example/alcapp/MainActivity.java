50f1653b010659ed2e8f263a8637ce2d7af05d90
Joshua Isaac <jayisaac0@gmail.com>
2019-07-14 20:40:00 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/alcapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.alcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onAboutButtonClick(View v){
+        Intent intent = new Intent(getBaseContext(),   Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void onProfileButtonClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   Main3Activity.class);
+        startActivity(myIntent);
+    }
+}
